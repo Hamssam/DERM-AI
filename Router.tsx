68668a4f680deb8693e72a9d46c0ec5576e0e58e
Hamssam <168197899+Hamssam@users.noreply.github.com>
2024-08-20 11:32:41 +0300

    Add files via upload

---
+++ Router.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button, SafeAreaView, Settings } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Opening2 from './src/Opening2';
+import Opening from './src/Opening';
+import Main from './src/Main';
+import Profile from './src/Profile';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Camera from './src/Camera';
+import Advices from './src/Advices';
+import Setting from './src/Setting';
+import Login from './src/Login';
+import Sign from './src/Sign';
+
+
+const Tab = createBottomTabNavigator();
+
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen name="Opening" component={Opening} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Sign" component={Sign} />
+        <Stack.Screen name="Opening2" component={Opening2} />
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Camera" component={Camera} />
+        <Stack.Screen name="Advices" component={Advices}/>
+        <Stack.Screen name="Settings" component={Setting} />
+    </Stack.Navigator>
+  );
+}
+
+export default function RootComponent() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
